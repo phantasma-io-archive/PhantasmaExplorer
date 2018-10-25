0b326e1d28b0a57418bfe5221c99211df4bad774
Sergio Flores <relfos@gmail.com>
2018-10-25 14:11:59 +0100

    More home page improvements

--- PhantasmaExplorer/ViewModels/TransactionViewModel.cs
+++ PhantasmaExplorer/ViewModels/TransactionViewModel.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using Phantasma.Blockchain;
+using Phantasma.Blockchain.Contracts;
+using Phantasma.Blockchain.Contracts.Native;
+using Phantasma.Blockchain.Tokens;
+using Phantasma.Cryptography;
+using Phantasma.Numerics;
 using Phantasma.VM;
 
 namespace Phantasma.Explorer.ViewModels
@@ -16,11 +21,64 @@ namespace Phantasma.Explorer.ViewModels
         public string FromAddress { get; set; }
         public IEnumerable<EventViewModel> Events { get; set; }
         public IEnumerable<Instruction> Instructions { get; set; }
+        public string Description { get; set; }
 
         public static TransactionViewModel FromTransaction(BlockViewModel block, Transaction tx,
             List<EventViewModel> evts)
         {
             var disasm = new Disassembler(tx.Script); //Todo fix me
+
+            string description = null;
+
+            Token senderToken = null;
+            Address senderChain = Address.Null;
+            Address senderAddress = Address.Null;
+
+            Token receiverToken = null;
+            Address receiverChain = Address.Null;
+            Address receiverAddress = Address.Null;
+
+            BigInteger amount = 0;
+
+            //Nexus nexus = null;
+
+            foreach (var evt in tx.Events)
+            {
+                switch (evt.Kind) 
+                {
+                    case EventKind.TokenSend:
+                        {
+                            var data = evt.GetContent<TokenEventData>();
+                            amount = data.amount;
+                            senderAddress = evt.Address;
+                            senderChain = data.chainAddress;
+                            //senderToken = nexus.FindTokenBySymbol(data.symbol);
+                        }
+                        break;
+
+                    case EventKind.TokenReceive:
+                        {
+                            var data = evt.GetContent<TokenEventData>();
+                            amount = data.amount;
+                            receiverAddress = evt.Address;
+                            receiverChain = data.chainAddress;
+                            //receiverToken = nexus.FindTokenBySymbol(data.symbol);
+                        }
+                        break;
+                }
+            }
+
+            if (description == null)
+            {
+                if (amount > 0 && senderAddress != Address.Null && receiverAddress != Address.Null && senderToken != null && senderToken == receiverToken)
+                {
+                    description = $"{TokenUtils.ToDecimal(amount)} {senderToken.Symbol} sent from {senderAddress.Text} to {receiverAddress.Text}";
+                }
+                else
+                {
+                    description = "Custom transaction";
+                }
+            }
 
             return new TransactionViewModel
             {
@@ -32,6 +90,7 @@ namespace Phantasma.Explorer.ViewModels
                 FromAddress = "????",
                 FromName = "Anonymous",
                 Events = evts,
+                Description = description,
                 Instructions = disasm.GetInstructions()
             };
         }
