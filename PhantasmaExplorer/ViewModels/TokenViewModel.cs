06e7e7813267823a9154b759d40a9eb7cf0c8a0e
Bruno Freitas <bruno_freitas_2015@outlook.com>
2018-10-31 12:05:05 +0000

    added token flags fixed bug with blocks list

--- PhantasmaExplorer/ViewModels/TokenViewModel.cs
+++ PhantasmaExplorer/ViewModels/TokenViewModel.cs
@@ -14,8 +14,9 @@ namespace Phantasma.Explorer.ViewModels
         public decimal CurrentSupply { get; set; }
         public decimal Price { get; set; }
         public int Transfers { get; set; }
+        public TokenFlags Flags { get; set; }
 
-        public static TokenViewModel FromToken(Token token, string logoUrl, int transfers ,decimal price = 0m)
+        public static TokenViewModel FromToken(Token token, string logoUrl, int transfers, decimal price = 0m)
         {
             return new TokenViewModel
             {
@@ -26,8 +27,13 @@ namespace Phantasma.Explorer.ViewModels
                 Decimals = (int)token.Decimals,
                 Price = price,
                 Transfers = transfers,
+                Flags = token.Flags,
                 LogoUrl = logoUrl //todo
             };
         }
+
+        public bool IsTransferable => Flags.HasFlag(TokenFlags.Transferable);
+        public bool IsFungible => Flags.HasFlag(TokenFlags.Fungible);
+        public bool IsFinite => Flags.HasFlag(TokenFlags.Finite);
     }
 }
