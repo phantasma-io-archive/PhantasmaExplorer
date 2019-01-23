fe1fb58aaea4515c050d37cc52e7f1fb68adc0bb
Bruno Freitas <bruno_freitas_2015@outlook.com>
2019-01-23 12:59:26 +0000

    removed unecessary projects from explorer solution

--- PhantasmaExplorer/Controllers/APIController.cs
+++ PhantasmaExplorer/Controllers/APIController.cs
@@ -1,12 +1,11 @@
-﻿using Phantasma.API;
-using LunarLabs.Parser;
+﻿using LunarLabs.Parser;
 using Phantasma.Explorer.Persistance;
 
 namespace Phantasma.Explorer.Controllers
 {
     public class ApiController
     {
-        private readonly NexusAPI _api;
+       // private readonly NexusAPI _api;
 
         private readonly ExplorerDbContext _context;
 
@@ -15,94 +14,94 @@ namespace Phantasma.Explorer.Controllers
             //_context = context;
         }
 
-        public DataNode GetAccount(string addressText)
-        {
-            return APIUtils.FromAPIResult(_api.GetAccount(addressText));
-        }
+        //public DataNode GetAccount(string addressText)
+        //{
+        //    return APIUtils.FromAPIResult(_api.GetAccount(addressText));
+        //}
 
-        public DataNode GetAddressTransactions(string addressText)
-        {
-            return APIUtils.FromAPIResult(_api.GetAddressTransactions(addressText));
-        }
+        //public DataNode GetAddressTransactions(string addressText)
+        //{
+        //    return APIUtils.FromAPIResult(_api.GetAddressTransactions(addressText));
+        //}
 
-        public DataNode GetApps()
-        {
-            return APIUtils.FromAPIResult(_api.GetApps());
-        }
+        //public DataNode GetApps()
+        //{
+        //    return APIUtils.FromAPIResult(_api.GetApps());
+        //}
 
-        public DataNode GetBlockByHash(string blockHash)
-        {
-            return APIUtils.FromAPIResult(_api.GetBlockByHash(blockHash));
-        }
+        //public DataNode GetBlockByHash(string blockHash)
+        //{
+        //    return APIUtils.FromAPIResult(_api.GetBlockByHash(blockHash));
+        //}
 
-        public DataNode GetRawBlockByHash(string blockHash)
-        {
-            return APIUtils.FromAPIResult(_api.GetRawBlockByHash(blockHash));
-        }
+        //public DataNode GetRawBlockByHash(string blockHash)
+        //{
+        //    return APIUtils.FromAPIResult(_api.GetRawBlockByHash(blockHash));
+        //}
 
-        public DataNode GetBlockByHeight(string chain, uint height)
-        {
-            return APIUtils.FromAPIResult(_api.GetBlockByHeight(chain, height));
-        }
+        //public DataNode GetBlockByHeight(string chain, uint height)
+        //{
+        //    return APIUtils.FromAPIResult(_api.GetBlockByHeight(chain, height));
+        //}
 
-        public DataNode GetRawBlockByHeight(string chain, uint height)
-        {
-            return APIUtils.FromAPIResult(_api.GetRawBlockByHeight(chain, height));
-        }
+        //public DataNode GetRawBlockByHeight(string chain, uint height)
+        //{
+        //    return APIUtils.FromAPIResult(_api.GetRawBlockByHeight(chain, height));
+        //}
 
-        public DataNode GetBlockHeight(string chain)
-        {
-            return APIUtils.FromAPIResult(_api.GetBlockHeight(chain));
-        }
+        //public DataNode GetBlockHeight(string chain)
+        //{
+        //    return APIUtils.FromAPIResult(_api.GetBlockHeight(chain));
+        //}
 
-        public DataNode GetBlockTransactionCountByHash(string blockHash)
-        {
-            return APIUtils.FromAPIResult(_api.GetBlockTransactionCountByHash(blockHash));
-        }
+        //public DataNode GetBlockTransactionCountByHash(string blockHash)
+        //{
+        //    return APIUtils.FromAPIResult(_api.GetBlockTransactionCountByHash(blockHash));
+        //}
 
-        public DataNode GetChains()
-        {
-            return APIUtils.FromAPIResult(_api.GetChains());
-        }
+        //public DataNode GetChains()
+        //{
+        //    return APIUtils.FromAPIResult(_api.GetChains());
+        //}
 
-        public DataNode GetConfirmations(string txHash)
-        {
-            return APIUtils.FromAPIResult(_api.GetConfirmations(txHash));
-        }
+        //public DataNode GetConfirmations(string txHash)
+        //{
+        //    return APIUtils.FromAPIResult(_api.GetConfirmations(txHash));
+        //}
 
-        public DataNode GetTransactionByHash(string txHash)
-        {
-            return APIUtils.FromAPIResult(_api.GetTransaction(txHash));
-        }
+        //public DataNode GetTransactionByHash(string txHash)
+        //{
+        //    return APIUtils.FromAPIResult(_api.GetTransaction(txHash));
+        //}
 
-        public DataNode GetTransactionByBlockHashAndIndex(string blockHash, int index)
-        {
-            return APIUtils.FromAPIResult(_api.GetTransactionByBlockHashAndIndex(blockHash, index));
-        }
+        //public DataNode GetTransactionByBlockHashAndIndex(string blockHash, int index)
+        //{
+        //    return APIUtils.FromAPIResult(_api.GetTransactionByBlockHashAndIndex(blockHash, index));
+        //}
 
-        public DataNode GetTokens()
-        {
-            return APIUtils.FromAPIResult(_api.GetTokens());
-        }
+        //public DataNode GetTokens()
+        //{
+        //    return APIUtils.FromAPIResult(_api.GetTokens());
+        //}
 
-        public DataNode GetTokenBalance(string address, string tokenSymbol, string chain)
-        {
-            return APIUtils.FromAPIResult(_api.GetTokenBalance(address, tokenSymbol, chain));
-        }
+        //public DataNode GetTokenBalance(string address, string tokenSymbol, string chain)
+        //{
+        //    return APIUtils.FromAPIResult(_api.GetTokenBalance(address, tokenSymbol, chain));
+        //}
 
-        public DataNode GetTokenTransfers(string tokenSymbol)
-        {
-            return APIUtils.FromAPIResult(_api.GetTokenTransfers(tokenSymbol));
-        }
+        //public DataNode GetTokenTransfers(string tokenSymbol)
+        //{
+        //    return APIUtils.FromAPIResult(_api.GetTokenTransfers(tokenSymbol));
+        //}
 
-        public DataNode GetTokenTransferCount(string tokenSymbol)
-        {
-            return APIUtils.FromAPIResult(_api.GetTokenTransferCount(tokenSymbol));
-        }
+        //public DataNode GetTokenTransferCount(string tokenSymbol)
+        //{
+        //    return APIUtils.FromAPIResult(_api.GetTokenTransferCount(tokenSymbol));
+        //}
 
-        public DataNode SendRawTransaction(string signedTx)
-        {
-            return APIUtils.FromAPIResult(_api.SendRawTransaction(signedTx));
-        }
+        //public DataNode SendRawTransaction(string signedTx)
+        //{
+        //    return APIUtils.FromAPIResult(_api.SendRawTransaction(signedTx));
+        //}
     }
 }
